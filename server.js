4a4f67a40c15becffa8b6ef070a5d8bf7b5bc086
Jean Carlos Penas <jeanpenas.vixsystem@vixsystem.com.br>
2021-10-21 00:31:47 -0300

    configuração para heroku.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/RPN-ANGULAR`));
+app.use(express.static(`${__dirname}/dist/app-tabuleiro-dama`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/RPN-ANGULAR/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/app-tabuleiro-dama/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
